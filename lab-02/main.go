30ecc1281d9515c776a3707c7a68b5c0831a3a95
Nouman Nisar <nnisar.official@gmail.com>
2026-06-20 19:18:22 +0500

    Upload lab-01 through lab-04

---
+++ lab-02/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := ""
+	fmt.Print("Enter your name: ")
+	fmt.Scan(&name)
+	fmt.Print("Hello, ", name, "!")
+}
---
+++ lab-04/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var num1, num2 float64
+	var operator string
+
+	fmt.Print("Enter first number: ")
+	fmt.Scan(&num1)
+
+	fmt.Print("Enter operator (+, -, *, /): ")
+	fmt.Scan(&operator)
+
+	fmt.Print("Enter first number: ")
+	fmt.Scan(&num2)
+
+	switch operator {
+	case "+":
+		fmt.Print(num1, " + ", num2, " = ", num1+num2)
+	case "-":
+		fmt.Print(num1, " - ", num2, " = ", num1-num2)
+	case "*":
+		fmt.Print(num1, " * ", num2, " = ", num1*num2)
+	case "/":
+		if num2 != 0 {
+			fmt.Print(num1, " / ", num2, " = ", num1/num2)
+		} else {
+			fmt.Print("Error: cannot divide by zero")
+		}
+	default:
+		fmt.Print("Unknown operator. Please use operator (+, -, *, /): ")
+	}
+
+}
