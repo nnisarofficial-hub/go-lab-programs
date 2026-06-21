bd61cfb1346fa3e527ecc2cfc913d0bcf63c00c5
Nouman Nisar <nnisar.official@gmail.com>
2026-06-21 14:59:17 +0500

    Simple Interest Calculator

--- lab-09/main.go
+++ lab-09/main.go
@@ -3,15 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var principal float64
+	var principal, annualRate, time float64
 	fmt.Print("Principal (PKR): ")
 	fmt.Scan(&principal)
 
-	var annualRate float64
 	fmt.Print("Annual rate (%): ")
 	fmt.Scan(&annualRate)
 
-	var time float64
 	fmt.Print("Time (years): ")
 	fmt.Scan(&time)
 
