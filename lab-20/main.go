b96d9e9c61896a54c1cd31ea9a3e60084224e229
nnisarofficial-hub <nnisar.official@gmail.com>
2026-06-28 16:41:48 +0500

    Update lab-20/main.go
    Co-authored-by: Faisal Nisar <faisal.nisar@careem.com>

--- lab-20/main.go
+++ lab-20/main.go
@@ -14,7 +14,6 @@ func main() {
 	if length1 >= (length2+length3) || length2 >= (length1+length3) || length3 >= (length1+length2) {
 		fmt.Print("Not a valid triangle")
 	} else if length1 == length2 && length2 == length3 {
-
 		fmt.Print("Equilateral triangle")
 	} else if length1 == length2 || length2 == length3 || length1 == length3 {
 		fmt.Print("Isosceles triangle")
