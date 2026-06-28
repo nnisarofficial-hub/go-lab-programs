54169ffcd9ca42e483e7963c44ae3cd2f4045234
Nouman Nisar <nnisar.official@gmail.com>
2026-06-28 16:38:59 +0500

    Vowel or Consonant

--- lab-19/main.go
+++ lab-19/main.go
@@ -1,16 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var alphabet string
 	fmt.Print("Enter a letter: ")
 	fmt.Scan(&alphabet)
 
-	switch alphabet {
-	case "A", "a", "E", "e", "I", "i", "O", "o", "U", "u":
+	lowerCase := strings.ToLower(alphabet)
+	switch lowerCase {
+	case "a", "e", "i", "o", "u":
 		fmt.Print(alphabet, " is a vowel")
-	case "B", "b", "C", "c", "D", "d", "F", "f", "G", "g", "H", "h", "J", "j", "K", "k", "L", "l", "M", "m", "N", "n", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z":
+	case "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z":
 		fmt.Print(alphabet, " is a consonant")
 	default:
 		fmt.Print(alphabet, " Please write an alphabet")
