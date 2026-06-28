f2ba90336698dac4baa130a4f6ed1297f6bd7b1a
Nouman Nisar <nnisar.official@gmail.com>
2026-06-29 03:08:30 +0500

    Number Guessing Game

---
+++ lab-30/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	const number = 42
+	counter := 0
+	for number != 0 {
+		var guess int
+		fmt.Print("Guess the number: ")
+		_, err := fmt.Scan(&guess)
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		if guess > number {
+			fmt.Println("Too high")
+			counter += 1
+		} else if guess < number {
+			fmt.Println("Too low")
+			counter += 1
+		} else if guess == number {
+			counter += 1
+			fmt.Printf("Correct! You got it in %d attempts.", counter)
+			break
+		}
+	}
+}
