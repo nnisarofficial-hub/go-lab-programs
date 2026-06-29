b12f14bc4fe54c2264db2cad5d80116130178ac1
Nouman Nisar <nnisar.official@gmail.com>
2026-06-29 23:49:29 +0500

    Reverse a String

---
+++ lab-33/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter text: ")
+	text, err := reader.ReadString('\r')
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	fmt.Print(reverseStringShort(text))
+
+}
+
+func reverseStringShort(s string) string {
+	runes := []rune(s)
+	slices.Reverse(runes)
+	return string(runes)
+}
