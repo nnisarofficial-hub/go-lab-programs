6cb1c5b27e5adbb03dec8fa18ab3c0b9112164f8
Nouman Nisar <nnisar.official@gmail.com>
2026-07-01 01:51:49 +0500

    Title Case

---
+++ lab-37/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter sentence: ")
+	sentence, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+	fmt.Print(titleCase(sentence))
+
+}
+func titleCase(sentence string) string {
+	words := strings.Fields(sentence)
+	for i, word := range words {
+		words[i] = strings.ToUpper(string(word[0])) + word[1:]
+	}
+	return strings.Join(words, " ")
+
+}
