22baa5a4406302204d1f150231c3624669d011e3
nnisarofficial-hub <nnisar.official@gmail.com>
2026-07-02 15:27:59 +0500

    Improve output formatting and variable naming

--- lab-41/main.go
+++ lab-41/main.go
@@ -6,13 +6,13 @@ func main() {
 	arr := [7]int{3, 1, 4, 5, 9, 2, 6}
 	min, max := minMax(arr)
 	fmt.Println("Numbers", arr)
-	fmt.Println("Minimum", min)
-	fmt.Println("Maximum", max)
+	fmt.Println("Minimum: ", min)
+	fmt.Println("Maximum: ", max)
 }
 
-func minMax(arr [7]int) (int, int) {
-	min := arr[0]
-	max := arr[0]
+func minMax(arr [7]int) (min int, max int) {
+	min = arr[0]
+	max = arr[0]
 	for _, val := range arr {
 		if val > max {
 			max = val
