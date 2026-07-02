3458834860f4de4a0ed88d0f45630555b11a40c3
nnisarofficial-hub <nnisar.official@gmail.com>
2026-07-02 15:16:48 +0500

    Update lab-39/main.go
    Co-authored-by: Faisal Nisar <faisal.nisar@careem.com>

--- lab-39/main.go
+++ lab-39/main.go
@@ -18,6 +18,7 @@ func main() {
 	fmt.Print(removeSpaces(sentence))
 	fmt.Printf("Removed %d spaces", countSpaces(sentence))
 }
+
 func countSpaces(sentence string) int {
 	return strings.Count(sentence, " ")
 }
