53a30fc2b833576d7ca02a6e527893123590383d
Nouman Nisar <nnisar.official@gmail.com>
2026-07-02 13:16:13 +0500

    min and max

---
+++ lab-41/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [7]int{3, 1, 4, 5, 9, 2, 6}
+	min, max := minMax(arr)
+	fmt.Println("Numbers", arr)
+	fmt.Println("Minimum", min)
+	fmt.Println("Maximum", max)
+}
+
+func minMax(arr [7]int) (int, int) {
+	min := arr[0]
+	max := arr[0]
+	for _, val := range arr {
+		if val > max {
+			max = val
+		} else if val < min {
+			min = val
+		}
+	}
+	return min, max
+}
