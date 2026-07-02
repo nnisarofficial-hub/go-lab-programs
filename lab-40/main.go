3517ecad31f0dff414205d91145f7c8bcd110d79
nnisarofficial-hub <nnisar.official@gmail.com>
2026-07-02 15:24:10 +0500

    Update lab-40/main.go
    Co-authored-by: Faisal Nisar <faisal.nisar@careem.com>

--- lab-40/main.go
+++ lab-40/main.go
@@ -23,7 +23,7 @@ func main() {
 		return
 	}
 	character = strings.TrimSpace(character)
-	fmt.Printf("'%s' appears %d times in %s", character, countChr(sentence, character), sentence)
+	fmt.Printf("'%s' appears %d times in \"%s\"", character, countChr(sentence, character), sentence)
 }
 
 func countChr(sentence, character string) int {
