7f596749dab2c4d9b3321f7d03dc663cefb94573
Nouman Nisar <nnisar.official@gmail.com>
2026-07-06 21:36:13 +0500

    lab-46 changes done

--- lab-46/main.go
+++ lab-46/main.go
@@ -3,51 +3,41 @@ package main
 import "fmt"
 
 func main() {
-	studentData()
-}
-
-type student struct {
-	Name  string
-	Score float64
-}
-
-func studentData() {
-	studentsData := []student{
+	studentsData := []Student{
 		{Name: "Ali", Score: 88.5},
 		{Name: "Sara", Score: 95.0},
 		{Name: "Umar", Score: 72.0},
 		{Name: "Fatima", Score: 61.5},
 		{Name: "Hassan", Score: 45.0},
 	}
-	finalGrades := grades(studentsData)
 	var totalScore float64
 	fmt.Printf("%-12s %-8s %s\n", "Name", "Score", "Grade")
-	for i, std := range studentsData {
-		scoreStr := fmt.Sprintf("%.1f", std.Score)
-		fmt.Printf("%-12s %-8s %s\n", std.Name, scoreStr, finalGrades[i])
+	for _, std := range studentsData {
+		// scoreStr := fmt.Sprintf("%.1f", std.Score)
+		fmt.Printf("%-12s %-8.1f %s\n", std.Name, std.Score, std.Grade())
 		totalScore += std.Score
 	}
 	fmt.Printf("\nClass average: %.2f\n", totalScore/float64(len(studentsData)))
 }
 
-func grades(list []student) []string {
-	var gradesList []string
-	for _, std := range list {
-		var letterGrade string
-		if std.Score >= 90 && std.Score <= 100 {
-			letterGrade = "A"
-		} else if std.Score >= 80 && std.Score <= 89 {
-			letterGrade = "B"
-		} else if std.Score >= 70 && std.Score <= 79 {
-			letterGrade = "C"
-		} else if std.Score >= 60 && std.Score <= 69 {
-			letterGrade = "D"
-		} else if std.Score < 60 && std.Score >= 0 {
-			letterGrade = "F"
-		} else if std.Score > 100 || std.Score < 0 {
-			fmt.Print("Invalid score. Must be between 0 and 100.")
-		}
-		gradesList = append(gradesList, letterGrade)
+type Student struct {
+	Name  string
+	Score float64
+}
+
+func (s Student) Grade() string {
+	if s.Score >= 90 && s.Score <= 100 {
+		return "A"
+	} else if s.Score >= 80 && s.Score <= 89 {
+		return "B"
+	} else if s.Score >= 70 && s.Score <= 79 {
+		return "C"
+	} else if s.Score >= 60 && s.Score <= 69 {
+		return "D"
+	} else if s.Score < 60 && s.Score >= 0 {
+		return "F"
+	} else if s.Score > 100 || s.Score < 0 {
+		return "Invalid score. Must be between 0 and 100."
 	}
-	return gradesList
+	return "Invalid"
 }
